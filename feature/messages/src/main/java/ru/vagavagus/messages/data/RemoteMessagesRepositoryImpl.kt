2805d0ea3e3054cc75f693f9c195f3ae2fcea8c3
Vladimir Efimov <ieifhs111@gmail.com>
2024-10-06 03:23:49 +0300

    add(feature-messages): updated mock repository implementation

--- feature/messages/src/main/java/ru/vagavagus/messages/data/RemoteMessagesRepositoryImpl.kt
+++ feature/messages/src/main/java/ru/vagavagus/messages/data/RemoteMessagesRepositoryImpl.kt
@@ -1,10 +1,13 @@
 package ru.vagavagus.messages.data
 
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 import kotlinx.datetime.Clock
 import kotlinx.datetime.DateTimeUnit
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.plus
+import ru.vagavagus.android_domain.wrappers.ResourceState
 import ru.vagavagus.messages.model.MessageReceive
 import ru.vagavagus.messages.model.MessageSend
 import ru.vagavagus.messages.repository.RemoteMessagesRepository
@@ -12,19 +15,20 @@ import ru.vagavagus.messages.repository.RemoteMessagesRepository
 internal class RemoteMessagesRepositoryImpl: RemoteMessagesRepository {
 
     /*Mock data with test delay(2000)*/
-    override suspend fun fetchReceivedMessages(): List<MessageReceive> {
+    override suspend fun fetchReceivedMessages(): ResourceState<List<MessageReceive>> = withContext(Dispatchers.IO){
         val message1 = MessageReceive(0, Clock.System.now().plus(1, DateTimeUnit.DAY, TimeZone.UTC).toEpochMilliseconds(), "Author1", "Hello, World!")
         val message2 = MessageReceive(1, Clock.System.now().plus(2, DateTimeUnit.MINUTE, TimeZone.UTC).toEpochMilliseconds(), "Author2", "Kotlin is awesome")
         val message3 = MessageReceive(2, Clock.System.now().plus(3, DateTimeUnit.HOUR, TimeZone.UTC).toEpochMilliseconds(), "Author3", "Testing frameworks")
         val message4 = MessageReceive(3, Clock.System.now().plus(4, DateTimeUnit.MINUTE, TimeZone.UTC).toEpochMilliseconds(), "Author4", "Unit testing")
-        val message5 = MessageReceive(4, Clock.System.now().plus(2, DateTimeUnit.WEEK, TimeZone.UTC).toEpochMilliseconds(), "Author5", "Integration testing")
+        val message5 = MessageReceive(4, Clock.System.now().plus(2, DateTimeUnit.WEEK, TimeZone.UTC).toEpochMilliseconds(), "Author5", "Integration testingIntegration testingIntegration testingIntegration testingIntegration testingIntegration testingIntegration testing")
         delay(2000)
 
-        return listOf(message1, message2, message3, message4, message5)
+        val items = listOf(message1, message2, message3, message4, message5)
+        ResourceState.Success(items)
     }
 
     /*Mock data with test delay(2000)*/
-    override suspend fun fetchSentMessages(): List<MessageSend> {
+    override suspend fun fetchSentMessages(): ResourceState<List<MessageSend>> = withContext(Dispatchers.IO){
         val message1 = MessageSend(0, Clock.System.now().plus(1, DateTimeUnit.DAY, TimeZone.UTC).toEpochMilliseconds(), "Author1", "Hello, World!")
         val message2 = MessageSend(1, Clock.System.now().plus(2, DateTimeUnit.MINUTE, TimeZone.UTC).toEpochMilliseconds(), "Author2", "Kotlin is awesome")
         val message3 = MessageSend(2, Clock.System.now().plus(3, DateTimeUnit.HOUR, TimeZone.UTC).toEpochMilliseconds(), "Author3", "Testing frameworks")
@@ -32,16 +36,18 @@ internal class RemoteMessagesRepositoryImpl: RemoteMessagesRepository {
         val message5 = MessageSend(4, Clock.System.now().plus(2, DateTimeUnit.WEEK, TimeZone.UTC).toEpochMilliseconds(), "Author5", "Integration testing")
         delay(3000)
 
-        return listOf(message1, message2, message3, message4, message5)
+        val items = listOf(message1, message2, message3, message4, message5)
+        ResourceState.Success(items)
     }
 
     /*Mock 5 authors*/
-    override suspend fun fetchAvailableRecipients(): List<String> {
-        return listOf("Author1", "Author2", "Author3", "Author4", "Author5")
+    override suspend fun fetchAvailableRecipients(): ResourceState<List<String>> = withContext(Dispatchers.IO){
+        val items = listOf("Author1", "Author2", "Author3", "Author4", "Author5")
+        ResourceState.Success(items)
     }
 
-    override suspend fun sendMessage(messageSend: MessageSend): Result<Unit> {
+    override suspend fun sendMessage(messageSend: MessageSend): Result<Unit> = withContext(Dispatchers.IO){
         /*We send our message somewhere and return the mock result*/
-        return Result.success(Unit)
+        Result.success(Unit)
     }
 }
