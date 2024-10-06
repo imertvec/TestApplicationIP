9ec20333f01e9d0fe7e92cabd9b6f83760a153f5
Vladimir Efimov <ieifhs111@gmail.com>
2024-10-06 03:21:28 +0300

    add(core-android-domain): minor update OrbitViewModel for ease access
    Update OrbitViewModel and add Resource wrapper, for ease state tracking

--- core/android-domain/src/main/java/ru/vagavagus/android_domain/orbit/OrbitViewModel.kt
+++ core/android-domain/src/main/java/ru/vagavagus/android_domain/orbit/OrbitViewModel.kt
@@ -1,4 +1,4 @@
-package ru.vagavagus.android_domain
+package ru.vagavagus.android_domain.orbit
 
 import androidx.lifecycle.ViewModel
 import org.orbitmvi.orbit.Container
@@ -8,6 +8,8 @@ import org.orbitmvi.orbit.viewmodel.container
 abstract class OrbitViewModel<STATE: Any, EVENT: Any, SIDE_EFFECT: Any>(
     initialState: STATE
 ) : ViewModel(), ContainerHost<STATE, SIDE_EFFECT> {
+    val uiState get() = container.stateFlow
+    val sideEffects get() = container.sideEffectFlow
 
     override val container: Container<STATE, SIDE_EFFECT> = container(initialState)
     abstract fun handleEvent(event: EVENT)
---
+++ core/android-domain/src/main/java/ru/vagavagus/android_domain/wrappers/ResourceState.kt
@@ -0,0 +1,9 @@
+package ru.vagavagus.android_domain.wrappers
+
+sealed class ResourceState <out T> {
+    data class Success<out T> (val data: T): ResourceState<T>()
+
+    /*We can put anything args to constructor, what we wish handle*/
+    data class Error (val errorMessage: String): ResourceState<Nothing>()
+    object Loading: ResourceState<Nothing>()
+}
