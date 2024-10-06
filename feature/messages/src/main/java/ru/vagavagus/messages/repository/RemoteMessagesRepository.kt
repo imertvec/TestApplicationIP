5abf14b4fd1165f22c0752dfa463d6c2cac00db4
Vladimir Efimov <ieifhs111@gmail.com>
2024-10-06 03:28:22 +0300

    add(feature-messages): updated `RemoteMessagesRepository` - wrapped returned values to ResourceState

--- feature/messages/src/main/java/ru/vagavagus/messages/repository/RemoteMessagesRepository.kt
+++ feature/messages/src/main/java/ru/vagavagus/messages/repository/RemoteMessagesRepository.kt
@@ -1,12 +1,13 @@
 package ru.vagavagus.messages.repository
 
+import ru.vagavagus.android_domain.wrappers.ResourceState
 import ru.vagavagus.messages.model.MessageReceive
 import ru.vagavagus.messages.model.MessageSend
 
 interface RemoteMessagesRepository {
-    suspend fun fetchReceivedMessages(): List<MessageReceive>
-    suspend fun fetchSentMessages(): List<MessageSend>
-    suspend fun fetchAvailableRecipients(): List<String>
+    suspend fun fetchReceivedMessages(): ResourceState<List<MessageReceive>>
+    suspend fun fetchSentMessages(): ResourceState<List<MessageSend>>
+    suspend fun fetchAvailableRecipients(): ResourceState<List<String>>
 
     /*Returns @Result<Unit> for simple check success/failure action*/
     suspend fun sendMessage(messageSend: MessageSend): Result<Unit>
